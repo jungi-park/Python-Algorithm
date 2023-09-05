973cfc16e30ca92b3918a34f3ade32648187430d
박준기 <qmqqqqm@gmail.com>
2023-09-05 22:42:51 +0900

    코드업 예제문제 30번까지 문제풀이

---
+++ coding/src/codeUp/Main.java
@@ -0,0 +1,85 @@
+package codeUp;
+
+import java.io.InputStreamReader;
+import java.text.Format;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Main {
+	//코드업 기초 100제
+	public static void main(String[] args) {
+/*		//1번 
+		System.out.println("Hello");
+		//2번 
+		System.out.println("Hello World");
+		//4번 
+		System.out.println("\'Hello\'");
+		//5번 
+		System.out.println("\"Hello World\"");
+		//6번 
+		System.out.println("\"!@#$%^&*()'");
+		//7번 
+		System.out.println("\"C:\\Download\'hello'.py\"");
+		//8번 
+		System.out.println("Hello\\nWorld");
+		//9번 
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();
+		System.out.println(text);
+		//10번 
+		Scanner sc1 = new Scanner(System.in);
+		int text1 = sc1.nextInt();
+		System.out.println(text1);
+		//11번 
+		Scanner sc1 = new Scanner(System.in);
+		float text1 = sc1.nextFloat();
+		System.out.println(text1);
+		//13번 
+		Scanner sc1 = new Scanner(System.in);
+		char text1 = sc1.next().charAt(0);
+		char text2 = sc1.next().charAt(0);
+		System.out.println(text2);
+		System.out.println(text1);
+		//15번
+		Scanner sc = new Scanner(System.in);
+		float f = sc.nextFloat();
+		System.out.printf("%.2f", f);
+		//17번
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.printf("%d %d %d", a, a, a);
+		//19번 
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();
+		String[] timeAll= text.split("\\.");
+		int y = Integer.parseInt(timeAll[0]);
+		int m = Integer.parseInt(timeAll[1]);
+		int d = Integer.parseInt(timeAll[2]);;
+		System.out.printf("%04d.%02d.%02d",y,m,d);
+		//20번 
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();
+		String[] num = text.split("-");
+		System.out.printf("%s%s",num[0],num[1]);
+		//21번
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();
+		System.out.println(text);
+		//24번
+		Scanner sc = new Scanner(System.in);
+		char[] text = sc.next().toCharArray();
+		for(char one:text) {
+			System.out.println(one);
+		}
+		//29번
+		Scanner sc = new Scanner(System.in);
+		Double text = sc.nextDouble();
+		System.out.printf("%.11f",text);
+		//30번
+		Scanner sc = new Scanner(System.in);
+		Long text = sc.nextLong();
+		System.out.printf("%d", text);
+ */
+	}
+
+}
---
+++ codingTest/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author parkjungi
+ *
+ */
+module codingTest {
+}
