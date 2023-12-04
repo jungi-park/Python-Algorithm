ba340d4081753bb0e40503bbd06387fdc55f5efd
jungi.park <qmqqqqm@gmail.com>
2023-12-04 18:51:00 +0900

    해시 포켓몬 문제풀이

---
+++ programmers/해시/포켓몬.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+def solution(nums):
+    answer = int(len(nums)/2)
+    nums = set(nums)
+    setNumsLen = len(nums)
+    if answer > setNumsLen:
+        return setNumsLen
+    return answer
