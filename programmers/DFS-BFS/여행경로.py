48829f8f7a378fe29f619084ea0cd24ce3418003
jungi.park <qmqqqqm@gmail.com>
2023-12-06 20:56:17 +0900

    DFS-BFS 여행경로 답안지 작성

--- programmers/DFS-BFS/여행경로.py
+++ programmers/DFS-BFS/여행경로.py
@@ -14,16 +14,45 @@ def solution(tickets):
             graph[start].append(end)
         else:
             graph[start] = []
-            graph[start].append(end) 
+            graph[start].append(end)
+
+    for k in graph.keys():
+         graph[k].sort(reverse = True)
 
     queue.append("ICN")
     while queue:
         s = queue.popleft()
         if s in graph:
-            for end in sorted(graph[s]):
+            for end in graph[s]:
                 if (s, end) not in visited:
                     queue.append(end)
                     visited.add((s, end))
         answer.append(s)
     
     return answer
+
+# 답안지
+def solution(tickets):
+    answer = []
+    routes = dict() # 티켓 정보를 저장하는 딕셔너리
+
+    for (start, end) in tickets:
+        if start in routes:
+            routes[start].append(end)
+        else:
+            routes[start] = [end]
+    # 갈 수 있는 공항을 알파벳 역순으로 정렬
+    for r in routes.keys():
+        routes[r].sort(reverse = True)
+
+    st = ["ICN"]
+    while st:
+        top = st[-1]
+        # 스택 top을 start로 하는 티켓이 없는 경우
+        if (top not in routes) or (not routes[top]):
+            answer.append(st.pop())   # top을 스택에서 꺼내서 answer에 저장
+        # 스택 top을 start로 하는 티켓이 있는 경우
+        else:
+            st.append(routes[top].pop())
+
+    return answer[::-1]
