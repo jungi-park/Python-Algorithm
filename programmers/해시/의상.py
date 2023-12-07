7c1febe4e0e8becd25e63b03dcb2e09510d3fde4
jungi.park <qmqqqqm@gmail.com>
2023-12-07 20:42:41 +0900

    해시 의상 문제풀이

---
+++ programmers/해시/의상.py
@@ -0,0 +1,15 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for clothe in clothes:
+        name , kind = clothe
+        if kind in dic:
+            dic[kind].append(name)
+        else:
+            dic[kind] = [name]
+      
+    for kind in dic:
+        # 해당 옷을 선택 안하는 경우도 옷으로 생각하면 +1
+        answer *= (len(dic[kind])+1)
+    # 모든 옷을 선택 안한 경우 즉, 옷을 1가지도 선택하지 않은 경우를 -1
+    return answer-1 
