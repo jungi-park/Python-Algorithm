8ab3fb337fa169d99a4c8be824b68d2d1e170e69
jungi.park <qmqqqqm@gmail.com>
2023-12-07 20:46:04 +0900

    해시 의상 주석 변경

--- programmers/해시/의상.py
+++ programmers/해시/의상.py
@@ -9,7 +9,7 @@ def solution(clothes):
             dic[kind] = [name]
       
     for kind in dic:
-        # 해당 옷을 선택 안하는 경우도 옷으로 생각하면 +1
+        # 해당 옷종류를 선택 안하는 경우도 옷으로 생각하면 +1
         answer *= (len(dic[kind])+1)
     # 모든 옷을 선택 안한 경우 즉, 옷을 1가지도 선택하지 않은 경우를 -1
     return answer-1 
