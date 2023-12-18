27320a3f8f5ac1174234befd99b9bb8735f2317e
jungi.park <qmqqqqm@gmail.com>
2023-12-18 21:19:34 +0900

    PCCP 기출문제 뒤에 있는 큰 수 찾기 문제풀이

---
+++ programmers/PCCP/뒤에 있는 큰 수 찾기.py
@@ -0,0 +1,26 @@
+# 내 풀이
+def solution(numbers):
+    answer = []
+    
+    for index,number in enumerate(numbers):
+        answer.append(-1)
+        for i in range(index,len(numbers)):
+            if number < numbers[i]:
+                answer.pop()
+                answer.append(numbers[i])
+                break
+        
+    return answer
+
+# 답
+def solution(numbers):
+    answer = [-1] * len(numbers)  # 초기값으로 -1로 채워진 리스트 생성
+    stack = []  # 인덱스를 저장할 스택
+
+    for i in range(len(numbers)):
+        while stack and numbers[stack[-1]] < numbers[i]:
+            answer[stack.pop()] = numbers[i]
+
+        stack.append(i)
+    
+    return answer
