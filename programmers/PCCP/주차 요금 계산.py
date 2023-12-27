8ab5962e35720be89faddfe33a6d2a3d970cfda9
jungi.park <qmqqqqm@gmail.com>
2023-12-27 17:14:50 +0900

    주차요금계산 sorted후 dict로 형변환 추가

--- programmers/PCCP/주차 요금 계산.py
+++ programmers/PCCP/주차 요금 계산.py
@@ -13,15 +13,15 @@ def solution(fees, records):
             h,m = map(int,record[0].split(":"))
             history[record[1]].append(h*60 + m)
     
-    history = sorted(history.items())
+    history = dict(sorted(history.items()))
     for h in history:
         useTime = 0
         fee = 0
-        while h[1]:
+        while history[h]:
             e = 23*60 + 59
-            s = h[1].pop(0)
-            if h[1]:
-                e = h[1].pop(0)
+            s = history[h].pop(0)
+            if history[h]:
+                e = history[h].pop(0)
             useTime += e - s
         
         if useTime <= fees[0]:
