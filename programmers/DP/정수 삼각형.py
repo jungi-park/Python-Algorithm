16d5692ed68896feda4f32b2e10f215b84125196
jungi.park <qmqqqqm@gmail.com>
2023-12-28 12:58:11 +0900

    정수삼각형 추가 작성

--- programmers/DP/정수 삼각형.py
+++ programmers/DP/정수 삼각형.py
@@ -1,3 +1,17 @@
+# 내답 시간초과
+def solution(triangle):
+  answer = 0
+  def recur(i,j,exVal):
+    dp[i] = max(dp[i],exVal+triangle[i][j])
+    if i+1 < len(triangle):
+      recur(i+1,j,exVal+triangle[i][j])
+    if j+1 < len(triangle[i]):
+      recur(i+1,j+1,exVal+triangle[i][j])
+  dp = [0] * len(triangle)
+  recur(0,0,0)
+  answer=dp[len(triangle)-1]
+  return answer
+
 def solution(triangle):
     memo ={}
     def recur(i,j):
