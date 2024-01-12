fb97e6ff0a41935dd8fdaa3b724b28b6639e53ea
jungi.park <qmqqqqm@gmail.com>
2024-01-12 17:36:25 +0900

    백준 문제 보물 풀이완료

---
+++ baekjoon/문제/보물.py
@@ -0,0 +1,14 @@
+result = 0
+n = int(input())
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+a.sort()
+b.sort(reverse=True)
+
+for i in range(n):
+  result =  result + (a[i]*b[i])
+
+print(result)
+  
