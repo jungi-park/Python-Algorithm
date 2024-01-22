059706a97714af5a2c41fdf3de553ef2bf57c244
jungi.park <qmqqqqm@gmail.com>
2024-01-22 20:22:21 +0900

    백준 브루트포스 수들의 합 2 문제풀이

---
+++ baekjoon/브루트포스/수들의 합 2.py
@@ -0,0 +1,21 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+
+left, right = 0, 1
+cnt = 0
+
+while right<=N and left<=right:
+  arry = nums[left:right]
+  total = sum(arry)
+
+  if total == M:
+    cnt+=1
+    right+=1
+  
+  elif total < M:
+    right+=1
+  
+  else:
+    left+=1
+
+print(cnt)
